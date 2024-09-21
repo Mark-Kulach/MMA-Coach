0b401e895f5701d61eadd0e184bb6a15ef2af98f
Mark-Kulach <mark@markkulach.com>
2024-09-21 12:02:39 -0500

    Made comments make sense to me

--- src/record.py
+++ src/record.py
@@ -10,18 +10,13 @@ Run this using PC in basement
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# Set up Chrome options
+# Set chrome profile to default
 chrome_options = Options()
-
-# Replace <YourUsername> with your actual Windows username
 chrome_options.add_argument(r"--user-data-dir=C:\\Users\\kulac\AppData\\Local\\Google\\Chrome\\User Data")
-chrome_options.add_argument("--profile-directory=Default")  # Change 'Default' if you use a different Chrome profile
+chrome_options.add_argument("--profile-directory=Default")
 
-# Optionally, set the path to chromedriver.exe if it's not in the PATH environment variable
-# driver = webdriver.Chrome(executable_path=r'C:\path\to\chromedriver.exe', options=chrome_options)
-
-# Initialize the WebDriver with the specified options
+# Open chrome
 driver = webdriver.Chrome(options=chrome_options)
 
-# Open the webpage
+# Go to FightPass
 driver.get('https://ufcfightpass.com/season/24054')
