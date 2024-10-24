e8595b7c50b7c59015389bf27f437345f6795830
Mark-Kulach <mark@markkulach.com>
2024-10-24 10:07:23 -0500

    more stuff

--- src/get_stats.py
+++ src/get_stats.py
@@ -2,11 +2,22 @@ import psycopg2
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
+from pyngrok import ngrok
+
 
 
 def connect_to_db():
-    DB_HOST = 'localhost'
-    DB_PORT = 5432
+    
+    if os.name == "nt":
+        ngrok_tunnel = ngrok.connect(5432)
+        public_url = ngrok_tunnel.public_url
+        DB_HOST, DB_PORT = public_url.split("://")[1].split(":")
+
+    else:
+        DB_HOST = 'localhost'
+        DB_PORT = 5432
+
     DB_NAME = 'mma_coach'
     DB_USER = 'postgres'
 
@@ -16,10 +27,7 @@ def connect_to_db():
     return conn, cursor
 
 
-def pull_events():
-    
-    conn, cursor = connect_to_db()
-    
+def pull_events(conn, cursor):    
     cursor.execute(f"SELECT title FROM events")
     rows = cursor.fetchall()
 
@@ -29,14 +37,14 @@ def pull_events():
     return [row[0] for row in rows]    
 
 
-def get_stats(driver):
+def get_stats(driver, conn, cursor):
     driver.get("http://ufcstats.com/statistics/events/completed?page=all")
 
     time.sleep(2)
     events = list(reversed(driver.find_elements(By.XPATH, "//a[contains(@href, 'ufcstats.com/event-details')]")))
-    events = {element.text: element.get_attribute("href") for element in events if element.text in pull_events()}
+    events = {element.text: element.get_attribute("href") for element in events if element.text in pull_events(conn, cursor)}
 
-
+    event_data = []
     for link in events.values():
         driver.get(link)
         time.sleep(2)
@@ -55,15 +63,16 @@ def get_stats(driver):
 
             fight_data = {"title": title}
 
-            del fighters
-
-            winner = None
+            outcome = "no contest"
             for el in fighter_el:
-                if el.find_element(By.XPATH, "../../../i").text == "W":
-                    winner = el.text
+                txt = el.find_element(By.XPATH, "../../../i").text
+                if txt == "W":
+                    outcome = "winner: " + el.text
+                elif txt == "D":
+                    outcome = "draw"
             
-            del fighter_el
-            
+            fight_data["outcome"] = outcome
+                        
             
             els = driver.find_elements(By.XPATH, "//i[contains(@class, 'b-fight-details__text-item')]")
             final_stats = [el.text for el in els]
@@ -71,7 +80,6 @@ def get_stats(driver):
             for i in range(len(final_stats)):
                 if final_stats[i][-1] == ":":
                     final_stats[i] = els[i].find_element(By.XPATH, "..").text
-            del els, i, el
 
             final_stats = {item.split(": ")[0].lower(): item.split(": ")[1].lower() for item in final_stats[:3]}
             final_stats["end_time"] = final_stats.pop("time")
@@ -79,7 +87,6 @@ def get_stats(driver):
 
             for toggle in driver.find_elements(By.XPATH, "//a[@class='b-fight-details__collapse-link_rnd js-fight-collapse-link']"):
                 toggle.click()
-            del toggle
 
             data_rows = "//thead[@class='b-fight-details__table-row b-fight-details__table-row_type_head']/following-sibling::*[1]/tr"
             data_rows = driver.find_elements(By.XPATH, data_rows)
@@ -88,18 +95,16 @@ def get_stats(driver):
             for i in range(len(data_rows)):
                 for j in range(len(data_rows[i])):
                     data_rows[i][j] = tuple(p.text for p in data_rows[i][j].find_elements(By.TAG_NAME, "p"))
-            del i, j
 
             num_rounds = len(data_rows) // 2
             totals = data_rows[:num_rounds]
             strikes = data_rows[num_rounds:]
-            del data_rows, num_rounds
 
             headers_totals = [None, "kd", "sig_strikes", None, "tot_strikes", "td", None, "sub_att", "rev", "ctrl"]
             headers_strikes = [None, None, None, "head", "body", "leg", "distance", "clinch", "ground"]
 
-            winner_stats = []
-            loser_stats = []
+            fighter_1 = []
+            fighter_2 = []
             for i in range(2):
                 name = totals[0][0][i]
     
@@ -123,20 +128,18 @@ def get_stats(driver):
                             else:
                                 temp[headers_strikes[stat]] = ok
 
-                    if name == winner:
-                        winner_stats.append(temp)
-                        fight_data["winner"] = name
-                    else:
-                        loser_stats.append(temp)
-                        fight_data["loser"] = name
+                if fighters.index(name) == 0:
+                    fighter_1.append(temp)
+                else:
+                    fighter_2.append(temp)
 
-            fight_data["winner_stats"] = winner_stats
-            fight_data["loser_stats"] = loser_stats
 
-            del headers_strikes, headers_totals, final_stats, i, stat, round, name, winner, strikes, totals, title, winner_stats, loser_stats
+            fight_data["fighter_1"] = fighter_1
+            fight_data["fighter_2"] = fighter_2
 
             fights.append(fight_data)
-        return fights
+        event_data.append(fights)
+    return event_data
 
 
 def format_stat(stat):
@@ -154,107 +157,80 @@ def format_stat(stat):
     return stat
 
 
-def push_to_db(data):
-    conn, cursor = connect_to_db()
-    for fight_data in data:
-        base_title = fight_data['title']
-        fight_title = base_title
-        counter = 1
+def push_to_db(data, conn, cursor):
+    for event_data in data:
+        for fight_data in event_data:
+            base_title = fight_data['title']
+            fight_title = base_title
+            counter = 1
 
-        while True:
-            cursor.execute("""
-                SELECT COUNT(*) FROM fights WHERE title = %s
-            """, (fight_title,))
-            count = cursor.fetchone()[0]
+            while True:
+                cursor.execute("""
+                    SELECT COUNT(*) FROM fights WHERE title = %s
+                """, (fight_title,))
+                count = cursor.fetchone()[0]
 
-            if count == 0:
-                break
-            else:
-                fight_title = f"{base_title} {counter}"
-                counter += 1
-
-        cursor.execute("""
-            INSERT INTO fights (title, winner, method, round, end_time)
-            VALUES (%s, %s, %s, %s, %s)
-            RETURNING id
-        """, (fight_title, fight_data['winner'], fight_data['method'], fight_data['round'], fight_data['end_time']))
-
-        fight_id = cursor.fetchone()[0]
-
-        for round in range(len(fight_data["winner_stats"])):
-            w = fight_data["winner_stats"][round]
+                if count == 0:
+                    break
+                else:
+                    fight_title = f"{base_title} {counter}"
+                    counter += 1
 
             cursor.execute("""
-                    INSERT INTO stats (fight_id, round, winner, sig_strikes, sig_strikes_att, tot_strikes, tot_strikes_att, td, td_att, sub_att, rev, ctrl, head, head_att, body, body_att, leg, leg_att, distance, distance_att, clinch, clinch_att, ground, ground_att)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """, (fight_id, 
-                      round + 1, 
-                      True,
-                      w["sig_strikes"],
-                      w["sig_strikes_att"],
-                      w["tot_strikes"],
-                      w["tot_strikes_att"],
-                      w["td"],
-                      w["td_att"],
-                      w["sub_att"],
-                      w["rev"],
-                      w["ctrl"],
-                      w["head"],
-                      w["head_att"],
-                      w["body"],
-                      w["body_att"],
-                      w["leg"],
-                      w["leg_att"],
-                      w["distance"],
-                      w["distance_att"],
-                      w["clinch"],
-                      w["clinch_att"],
-                      w["ground"],
-                      w["ground_att"])
-            )
+                INSERT INTO fights (title, winner, method, round, end_time)
+                VALUES (%s, %s, %s, %s, %s)
+                RETURNING id
+            """, (fight_title, fight_data['winner'], fight_data['method'], fight_data['round'], fight_data['end_time']))
 
-        
-        for round in range(len(fight_data["loser_stats"])):
-            l = fight_data["loser_stats"][round]
+            conn.commit()
 
-            cursor.execute("""
-                    INSERT INTO stats (fight_id, round, winner, sig_strikes, sig_strikes_att, tot_strikes, tot_strikes_att, td, td_att, sub_att, rev, ctrl, head, head_att, body, body_att, leg, leg_att, distance, distance_att, clinch, clinch_att, ground, ground_att)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """, (fight_id, 
-                      round + 1, 
-                      False, 
-                      l["sig_strikes"],
-                      l["sig_strikes_att"],
-                      l["tot_strikes"],
-                      l["tot_strikes_att"],
-                      l["td"],
-                      l["td_att"],
-                      l["sub_att"],
-                      l["rev"],
-                      l["ctrl"],
-                      l["head"],
-                      l["head_att"],
-                      l["body"],
-                      l["body_att"],
-                      l["leg"],
-                      l["leg_att"],
-                      l["distance"],
-                      l["distance_att"],
-                      l["clinch"],
-                      l["clinch_att"],
-                      l["ground"],
-                      l["ground_att"])
-            )
+            fight_id = cursor.fetchone()[0]
 
-    conn.commit()
+            for i in ("1", "2"):
+
+                for round in range(len(fight_data["fighter_" + i])):
+                    w = fight_data["fighter_" + i][round]
+
+                    cursor.execute("""
+                            INSERT INTO stats (fight_id, round, fighter, sig_strikes, sig_strikes_att, tot_strikes, tot_strikes_att, td, td_att, sub_att, rev, ctrl, head, head_att, body, body_att, leg, leg_att, distance, distance_att, clinch, clinch_att, ground, ground_att)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        """, (fight_id, 
+                            round + 1,
+                            i, 
+                            w["sig_strikes"],
+                            w["sig_strikes_att"],
+                            w["tot_strikes"],
+                            w["tot_strikes_att"],
+                            w["td"],
+                            w["td_att"],
+                            w["sub_att"],
+                            w["rev"],
+                            w["ctrl"],
+                            w["head"],
+                            w["head_att"],
+                            w["body"],
+                            w["body_att"],
+                            w["leg"],
+                            w["leg_att"],
+                            w["distance"],
+                            w["distance_att"],
+                            w["clinch"],
+                            w["clinch_att"],
+                            w["ground"],
+                            w["ground_att"])
+                    )
+
+                    conn.commit()
+
     cursor.close()
     conn.close()
 
 
 def main():
     driver = webdriver.Chrome()
-    push_to_db(get_stats(driver))
-    
+    conn, cursor = connect_to_db()
+    push_to_db(get_stats(driver, conn, cursor), conn, cursor)
+    driver.quit()
 
 if __name__ == "__main__":
     main()
